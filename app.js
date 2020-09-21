1c5e4d3c4bdca90d1813ff0af8f5e89384649f84
malekshmes <malekshmes@gmail.com>
2020-09-22 00:47:13 +0300

    lab12 added

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 'use strict';
 // testing the js file
 // console.log('hello I am working')
+var ctx = document.getElementById('productChart').getContext('2d');
+var xlabels= [];
+
+var ynumbers= [];
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: xlabels,
+    datasets: [{
+      label: 'Number Of Clicks',
+      data: ynumbers,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+      ],
+    }]
+  },
+});
+console.log(myChart);
 var firstImage = document.getElementById('first-image');
 var secondImage = document.getElementById('second-image');
 var thirdImage = document.getElementById('third-image');
 
-
+// var testArray =[-1,-1,-1];
 
 var selections = [];
 console.log(selections);
@@ -17,28 +35,49 @@ function Product(name, link) {
   this.dispalyTimes = 0;
 
   selections.push(this);
+  
 }
 var firstImageIndex = 0;
 var secondImageIndex = 0;
 var thirdImageIndex = 0;
 
 function getRandomImage() {
+  // the new conditions for the testArray lab12 not working
+  // do {
+  //   firstImageIndex = Math.floor((Math.random() * selections.length));
+  // }while (firstImageIndex === testArray[0] || secondImageIndex === testArray[1] || thirdImageIndex === testArray[2]);
+  // do {
+  //   secondImageIndex = Math.floor((Math.random() * selections.length));
+  // }
+  // while (secondImageIndex === firstImageIndex && secondImageIndex === testArray[0] || secondImageIndex === testArray[1] || secondImageIndex === testArray[2] );
 
+  // do {
+  //   thirdImageIndex = Math.floor(Math.random() * selections.length);
+  //   while (thirdImageIndex === secondImageIndex && thirdImageIndex === firstImageIndex);}
+
+  // while ( thirdImageIndex === testArray[0] || thirdImageIndex === testArray[1] || thirdImageIndex === testArray[2] );{
+  //   thirdImageIndex = Math.floor(Math.random() * selections.length);
+
+  // }
   firstImageIndex = Math.floor((Math.random() * selections.length));
   do {
     secondImageIndex = Math.floor((Math.random() * selections.length));
   }
   while (firstImageIndex === secondImageIndex);
+  do{
+    thirdImageIndex = Math.floor((Math.random() * selections.length));
+  }
+  while (thirdImageIndex === secondImageIndex || thirdImageIndex === firstImageIndex );
 
-  do {
-    thirdImageIndex = Math.floor(Math.random() * selections.length);
-  } while (thirdImageIndex === secondImageIndex || thirdImageIndex === firstImageIndex);
   console.log(firstImageIndex);
   console.log(secondImageIndex);
   console.log(thirdImageIndex);
   displayImages(firstImageIndex, secondImageIndex, thirdImageIndex);
+  // testArray=[];
+  // testArray.push(firstImageIndex, secondImageIndex, thirdImageIndex);
 
 }
+// firstImageIndex
 var currentRightImage;
 var currentCenteredImage;
 var currentLeftImage;
@@ -87,6 +126,7 @@ function calculateSelections(event) {
   if (event.target.id === 'first-image') {
     currentRightImage.clicks++;
     totalClicks++;
+
     getRandomImage();
   } else if (event.target.id === 'second-image') {
     currentCenteredImage.clicks++;
@@ -106,13 +146,16 @@ function calculateSelections(event) {
 
 }
 // console.log(currentRightImage.clicks);
-
 var ulEL = document.getElementById('list');
 function displayResults() {
   for (var i = 0; i < selections.length; i++) {
     var listItems = document.createElement('li');
     ulEL.appendChild(listItems);
-    listItems.textContent =  selections[i].name +'Number of votes ' + selections[i].clicks + '. Displaying times for this image ' + selections[i].dispalyTimes;
+    listItems.textContent = selections[i].name +' Number of votes ' + selections[i].clicks + '. Displaying times for this image ' + selections[i].dispalyTimes;
+    var productName = selections[i].name;
+
+    xlabels.push(productName);
   }
 
 }
+
